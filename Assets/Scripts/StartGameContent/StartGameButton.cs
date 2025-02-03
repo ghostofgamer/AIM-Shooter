cd2c69dc392cea388aebd56c7f55fb494e176e66
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-02-03 17:38:49 +0400

    AddStartGameRefactoring

--- Assets/Scripts/StartGameContent/StartGameButton.cs
+++ Assets/Scripts/StartGameContent/StartGameButton.cs
@@ -1,10 +1,12 @@
-using StartGameContent;
 using UnityEngine;
 
-public class StartGameButton : MonoBehaviour, ISettingsHandler
+namespace StartGameContent
 {
-    [SerializeField] private DifficultySettings _difficultySettings;
-    [SerializeField] private StartGame _startGame;
+    public class StartGameButton : MonoBehaviour, ISettingsHandler
+    {
+        [SerializeField] private DifficultySettings _difficultySettings;
+        [SerializeField] private StartGame _startGame;
 
-    public void SetSettings() => _startGame.Play(_difficultySettings);
+        public void SetSettings() => _startGame.Play(_difficultySettings);
+    }
 }
